30f5fa4d876e11590eea7562db857e1710e4ed75
William Barela <william.barela.dev@gmail.com>
2018-05-21 11:51:45 -0500

    modified arithmetic to correct errors

--- parser.sh
+++ parser.sh
@@ -14,7 +14,7 @@
 # split key file into number of process files as temporty files.
 # start a prcoess for each one of them.
 
-# These steps on 36 processors should reduce the run time from 50 hours to about 2 hours
+# These steps on 36 processors should reduce the run time to about 2 minutes.
 # FIXME: Need to add one liner to remove cariage returns
 
 # get argvs
@@ -59,7 +59,7 @@ function echo_info () {
 
 	echo " "
 
-	echo "[ PAIRS ]"
+	echo "[ PROCESSOR PAIRS ]"
 	for i in $(seq 0 1 $((seqs_length - 2))); do 
 		let "x=$i"
 		let "j=$((x + 1))"
@@ -69,22 +69,21 @@ function echo_info () {
 	done
 }
 
-# function process_child { args : integer input } {
-# 	let "i=${input}"
-#	let "j=$(($i + 1))"
-#	let "seq_start=${seqs[$i]}"
-#	let "seq_end=$((seqs[$j] - 1))"
-#
-# 	partial_search=$(sed -n "$seq_start","$seq_end"p "$clean_repeats_file");
-# 	for search in $partial_search; do
-# 		# FIXME: check if count is not null then echo to output file
-# 		count=$(grep -o $search $genome_file | uniq -c);
-# 		echo "$genome_file $count" >> $output_file;
-# 	done
-# }
-# 
-# for i in $(seq 0 1 $((seqs_length - 2))); do
-# 	process_child $i &
-# done
+function process_child () {
+	let "i=$1"
+	let "j=$(($i + 1))"
+	let "seq_start=${seqs[$i]}"
+	let "seq_end=$((seqs[$j] - 1))"
+
+	partial_search=$(sed -n "$seq_start","$seq_end"p "$clean_repeats_file");
+	for search in $partial_search; do
+		count=$(grep -o $search $genome_file | uniq -c);
+		[[ ! -z "$count" ]] && echo "$genome_file $count process_$j" >> $output_file;
+	done
+}
+
+for i in $(seq 0 1 $((seqs_length - 2))); do
+	process_child $i &
+done
 
 echo_info
